9b28115f91b10021f185702d2fdfe8b71290a0c6
wxzhang <42400958+zwx94@users.noreply.github.com>
2025-08-26 19:59:38 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -6,7 +6,7 @@
 # -- Project information -----------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#project-information
 
-project = 'CellfreeGMF'
+project = 'CellFreeGMF'
 copyright = '2025, Wenxiang Zhang'
 author = 'Wenxiang Zhang'
 release = 'v1'
