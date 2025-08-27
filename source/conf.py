848e05c3a92b39767f4ae708f6677992c1e81b7a
wxzhang <42400958+zwx94@users.noreply.github.com>
2025-08-27 18:18:45 +0800

    Update conf.py

--- source/conf.py
+++ source/conf.py
@@ -14,7 +14,7 @@ release = 'v1'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = ['sphinx_rtd_theme']
+extensions = ["nbsphinx", "sphinx_rtd_theme"]
 
 templates_path = ['_templates']
 exclude_patterns = []
